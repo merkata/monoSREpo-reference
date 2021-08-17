7c2e76e5b4c51a9b272c6b2d78d8be1d897791ae
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2021-08-17 23:03:08 +0300

    feat(infrastructure): Add k8s app module

---
+++ infrastructure/modules/k8s-app/main.tf
@@ -0,0 +1,45 @@
+data "aws_eks_cluster" "default" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.default.token
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  sensitive_content = templatefile("${path.module}/kubeconfig.tpl", {
+    cluster_name = var.cluster_name,
+    clusterca    = data.aws_eks_cluster.default.certificate_authority[0].data,
+    endpoint     = data.aws_eks_cluster.default.endpoint,
+    })
+  filename          = "./kubeconfig-${var.cluster_name}"
+}
+
+resource "kubernetes_namespace" "app" {
+  metadata {
+    name = var.app_name
+  }
+}
+
+resource "helm_release" "app" {
+  namespace  = kubernetes_namespace.app.metadata.0.name
+  wait       = true
+  timeout    = 600
+
+  name       = var.app_name
+  chart      = var.app_chart_path
+}
---
+++ infrastructure/modules/k8s-app/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "app_chart_path" {
+  type = string
+}
