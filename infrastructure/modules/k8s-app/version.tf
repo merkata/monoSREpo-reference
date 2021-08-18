5d3d06d1e27a32a06f4e12811295397c7ba86e0f
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2021-08-18 07:07:58 +0300

    chore(infrastructure): Bump aws version

--- infrastructure/modules/k8s-app/version.tf
+++ infrastructure/modules/k8s-app/version.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.1.0"
+      version = ">= 2.1.0"
     }
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.39.0"
+      version = ">= 3.40.0"
     }
   }
 }
